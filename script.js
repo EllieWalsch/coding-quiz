987a2f6b0ae5799eca328add1d233bc50ca31030
EllieWalsch <elliewalsch4@gmail.com>
2022-10-28 14:39:11 -0500

    Display start quiz with button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const display = document.querySelector();
+const startBtn = document.querySelector("#btn");
+
+startBtn.addEventListener("click", function(){
+    console.log("button clicked")
+})
+
+// TODO: display a start quiz screen with functional button
