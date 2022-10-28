f04a6fb94f9c942e4cd5c096ddc23e05c780b2ea
EllieWalsch <elliewalsch4@gmail.com>
2022-10-28 18:45:13 -0500

    Hide startQuiz when button is clicked

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const startBtn = document.querySelector("#btn");
-const quizContainer = document.querySelector(".quiz")
+
 
 startBtn.addEventListener("click", startQuiz)
 
-// TODO: when button is clicked, display something
-function startQuiz (){
-    document.querySelector(".quiz").innerHTML = "YOU CLICKED ME!";
+
+function startQuiz () {
+    document.querySelector("#header").style.display = "none";
+    document.querySelector("#btn").style.display = "none";
+    return startQuestions();
+
+
+}// TODO: when button is clicked, hide the start quiz screen and begin quiz
+function startQuestions () {
+    console.log("hello")
 }
+
+
+
+
+
