1ac9984aac20526a833822a95a5aa188247f1c1d
EllieWalsch <elliewalsch4@gmail.com>
2022-11-01 13:21:48 -0500

    WIP having trouble exporting question array

---
+++ src/lib.js
@@ -0,0 +1,36 @@
+// BUG - HAVING TROUBLE EXPORTING QUESTION ARRAY
+
+
+// export let questions = [
+//     {
+//         question : "Question 1?",
+
+//         choiceA : "Correct",
+
+//         choiceB : "Wrong",
+
+//         choiceC : "Wrong",
+
+//         correct : "A"
+//     },{
+//         question : "Question 2?",
+
+//         choiceA : "Wrong",
+
+//         choiceB : "Correct",
+
+//         choiceC : "Wrong",
+
+//         correct : "B"
+//     },{
+//         question : "Question 3?",
+
+//         choiceA : "Wrong",
+
+//         choiceB : "Wrong",
+
+//         choiceC : "Correct",
+
+//         correct : "C"
+//     }
+// ];
