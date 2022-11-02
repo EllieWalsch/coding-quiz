134c919020d9e115c26d8e09b99edece2624892c
EllieWalsch <elliewalsch4@gmail.com>
2022-11-01 22:03:27 -0500

    Add office hours fixes

--- src/script.js
+++ src/script.js
@@ -1,49 +1,20 @@
+import { questions } from "./lib.js"
 const startBtn = document.querySelector("#btn");
 const question = document.querySelector("#question");
-const choiceA = document.querySelector("#A");
-const choiceB = document.querySelector("#B");
-const choiceC = document.querySelector("#C");
-
-const questions = [
-    {
-        question : "Question 1?",
-
-        choiceA : "Correct",
-
-        choiceB : "Wrong",
-
-        choiceC : "Wrong",
-
-        correct : choiceA
-    },{
-        question : "Question 2?",
-
-        choiceA : "Wrong",
-
-        choiceB : "Correct",
-
-        choiceC : "Wrong",
-
-        correct : choiceB
-    },{
-        question : "Question 3?",
-
-        choiceA : "Wrong",
-
-        choiceB : "Wrong",
-
-        choiceC : "Correct",
-
-        correct : choiceC
-    }
-];
+const answerList = document.querySelector("#list");
 
 
 // Hide intro when quiz begins
 function hideIntro () {
-    document.querySelector("#header").style.display = "none";
+    document.querySelector("#header").classList.toggle("hidden");
     document.querySelector("#btn").style.display = "none";
-    return startQuestions();
+    startQuestions();
+}
+
+// Hide quiz at the very beginning (will have to do this at high score screen as well)
+function hideQuiz () {
+    document.querySelector("#quiz").style.display = "none";
+
 }
 
 // Begin at question index 0
@@ -52,30 +23,48 @@ let currentQuestionIndex = 0;
 
 // Replace text content with values from our questions array
 function startQuestions () {
+    document.querySelector("#quiz").style.display = "block";
     let currentQ = questions[currentQuestionIndex];
-    question.innerText = currentQ.question;
-    choiceA.innerText = currentQ.choiceA;
-    choiceB.innerText = currentQ.choiceB;
-    choiceC.innerText = currentQ.choiceC;
+    let questionH2 = document.getElementById("question");
+    questionH2.innerText = currentQ.question;
+    let answerList = document.getElementById("list");
+    answerList.innerText = currentQ.choices;
+
 }
+
 
 // Compare user answer with correct answer
 function checkAnswer(event) {
-    let userAns = event.target
+    let userAns = event.target.innerText;
     if (userAns === questions[currentQuestionIndex].correct) {
-        console.log("correct")
+        console.log("correct");
     } else {
-        console.log("wrong")
+        console.log("wrong");
+    }
+    // Add 1 to our currentQuestionIndex to show another question - TODO: if quiz is over, show high scores
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex ++;
+        startQuestions ();
+    } else {
+        console.log("end quiz");
     }
 }
 
-// TODO: Create timer - when startQuestions is called, begin countdown
+function countDown () {
+
+}
+
+// TODO: Make a timer REWATCH 10/27 lecture
+// TODO: If answer is wrong, subtract time
 
 
+
+// ****TODO: Put choices in a list so I don't have so many event listeners and query selectors?
+hideQuiz ();
 startBtn.addEventListener("click", hideIntro);
-choiceA.addEventListener("click", checkAnswer);
-choiceB.addEventListener("click", checkAnswer);
-choiceC.addEventListener("click", checkAnswer);
+document.querySelector("#list").addEventListener("click", checkAnswer);
+
+
 
 
 
