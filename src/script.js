b144f1fc13803ff05c1574cc484faee1b9195e3d
EllieWalsch <elliewalsch4@gmail.com>
2022-11-02 10:30:02 -0500

    Fix answer in questions array
    Add commas at ends

--- src/script.js
+++ src/script.js
@@ -1,15 +1,17 @@
 import { questions } from "./lib.js"
 const startBtn = document.querySelector("#btn");
-const question = document.querySelector("#question");
-const answerList = document.querySelector("#list");
+let questionH2 = document.getElementById("question");
+let answerList = document.getElementById("list");
 
 // TODO: make a const for all of my classes and ids
 
+// Begin on intro page
+hideQuiz ();
 
 // Hide intro when quiz begins
 function hideIntro () {
     document.querySelector("#header").classList.toggle("hidden");
-    document.querySelector("#btn").style.display = "none";
+    document.querySelector("#btn").classList.toggle("hidden");
     startQuestions();
 }
 
@@ -17,31 +19,35 @@ function hideIntro () {
 
 // Hide quiz at the very beginning (will have to do this at high score screen as well)
 function hideQuiz () {
-    document.querySelector("#quiz").style.display = "none";
-
+    document.querySelector("#quiz").classList.toggle("hidden");
 }
 
 // Begin at question index 0
 let currentQuestionIndex = 0;
 
 
-// TODO: rework below 
 // Replace text content with values from our questions array
 function startQuestions () {
     document.querySelector("#quiz").style.display = "block";
-    let currentQ = questions[currentQuestionIndex];
-    let questionH2 = document.getElementById("question");
-    questionH2.innerText = currentQ.question;
-    let answerList = document.getElementById("list");
-    answerList.innerText = currentQ.choices;
 
+    let currentQ = questions[currentQuestionIndex];
+
+    questionH2.innerText = currentQ.question;
+
+    answerList.innerHTML = "";
+
+    currentQ.choices.forEach((choice) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<button type="button">${choice}</button>`;
+        answerList.appendChild(li);
+    });
 }
 
 
 // Compare user answer with correct answer
 function checkAnswer(event) {
     let userAns = event.target.innerText;
-    if (userAns === questions[currentQuestionIndex].correct) {
+    if (userAns === questions[currentQuestionIndex].answer) {
         console.log("correct");
     } else {
         console.log("wrong");
@@ -64,10 +70,9 @@ function countDown () {
 
 
 
-// ****TODO: Put choices in a list so I don't have so many event listeners and query selectors?
-hideQuiz ();
 startBtn.addEventListener("click", hideIntro);
-document.querySelector("#list").addEventListener("click", checkAnswer);
+answerList.addEventListener("click", checkAnswer);
+
 
 
 
