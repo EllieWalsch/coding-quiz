7e372ccf799eea758106062d2602511ddaaae5ee
EllieWalsch <elliewalsch4@gmail.com>
2022-11-04 15:24:20 -0500

    Add more questions

--- src/lib.js
+++ src/lib.js
@@ -4,12 +4,32 @@ export const questions = [
             choices : [ "alerts", "booleans", "strings", "numbers" ],
             answer: "alerts",
         },{
-            question : "Question 2?",
-            choices : [ "correct", "wrong 1", "wrong 2", "wrong 3" ],
-            answer: "correct",
+            question : "A very useful tool used during development and debugging for printing content to the debugger is:",
+            choices : [ "JavaScript", "terminal/bash", "for loops", "console.log" ],
+            answer: "console.log",
         },{
-            question : "Question 3?",
-            choices : [ "correct", "wrong 1", "wrong 2", "wrong 3" ],
-            answer: "correct",
+            question : "Arrays in Javascript can be used to store...",
+            choices : [ "numbers", "strings", "booleans", "all of the above" ],
+            answer: "all of the above",
+        },{
+            question : "What does API stand for?",
+            choices : ["Awesome Party Individials", "Alert Placement Interval", "Application Programming Interface", "Application Process Index"],
+            answer : "Application Programming Interface",
+        },{
+            question : "What does DOM stand for?",
+            choices : [ "Document Object Model", "Dogs Operating Machinery", "Document Over Main", "Document Overloading Memory" ],
+            answer: "Document Object Model",
+        },{
+            question : "what does JSON.stringify() do?",
+            choices : [ "Converts a Javascript object into a boolean", "Converts a Javascript object into a string", "Blows up your computer", "Converts a Javascript object into a number" ],
+            answer: "Converts a Javascript object into a string",
+        },{
+            question : "The condition in an if / else statement is enclosed within",
+            choices : [ "quotes", "curly brackets", "parentheses", "square brackets" ],
+            answer: "parentheses",
+        }, {
+            question : "",
+            choices : [],
+            answer: "",
         }
     ];
